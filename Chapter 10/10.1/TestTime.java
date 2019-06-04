8bdc0211ed0782b7f11f4dcb76197952e7e1ab0a
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-03 21:48:24 -0400

    Update TestTime.java

--- Chapter 10/10.1/TestTime.java
+++ Chapter 10/10.1/TestTime.java
@@ -6,7 +6,7 @@ package time;
 
 public class TestTime {
 	public static void main(String[] args) {
-		Time time1 = new Time();					// no-arg object
+		Time time1 = new Time();				// no-arg object
 		Time time2 = new Time(555550000);			// arg object
 		time2 = time2.setTime(time2.second);
 		System.out.println(time1.totalHours + ":" + time1.totalMinutes + ":" + time1.totalSeconds);
