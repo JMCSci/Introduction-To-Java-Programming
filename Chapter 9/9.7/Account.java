0562dfe3d71af0e66352290c3e3ccf4071c5372d
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-05 16:27:57 -0400

    Update Account.java

--- Chapter 9/9.7/Account.java
+++ Chapter 9/9.7/Account.java
@@ -11,7 +11,7 @@ public class Account {
 	private double annualInterestRate;	// stores current interest rate (default 0)
 	private Date dateCreated;			// stores the date when account was created
 
-// constructor -- default account
+// constructor -- default account 
 	Account() {
 		id = 0;
 		balance = 0;
@@ -25,23 +25,23 @@ public class Account {
 		this.balance = balance;
 	}
 // accessor (getter) method
-	public int id(int id) {
+	public int getId() {
 		return id;
 	}
 // id mutator (setter) method	
-	public void setid(int newValue) {
+	public void setId(int newValue) {
 		id = newValue;
 	}
 // balance accessor (getter) method	
-	public double balance(double balance) {
+	public double getBalance() {
 		return balance;
 	}
 // balance mutator (setter) method	
 	public void setBalance(double newValue) {
 		balance = newValue;
 	}
-// annualInterestRate accessor (getter) method	
-	public double annualInterestRate(double annualInterestRate) {
+// annualInterestRate (getter) method	
+	public double annualInterestRate() {
 		return annualInterestRate;
 	}
 // annualInterestRate mutator (setter) method
@@ -55,22 +55,6 @@ public class Account {
 		return dateCreated;
 	}
 	
-/*  MAIN */
-	public static void main(String[] args) {
-		Account account = new Account();
-		double monthlyInterest;
-		account.id = 1122;
-		account.balance = 20000;
-		account.annualInterestRate = 0.045;
-		account.withdraw();
-		account.deposit(); 
-
-		monthlyInterest = account.getMonthlyInterest(account.getMonthlyInterestRate());
-		System.out.println("Account balance $" + account.balance + "\n" + 
-							"Monthly interest $" + monthlyInterest + "\n" + 
-							 "Account created: " + account.dateCreated());
-		
-	}
 // method returns monthly interest rate
 	public double getMonthlyInterestRate() {
 		return annualInterestRate / 12;
