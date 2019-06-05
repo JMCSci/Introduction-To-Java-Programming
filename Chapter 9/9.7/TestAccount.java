28a603ec670717970b1b368b1684a0abf9aedb68
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-05 16:34:39 -0400

    Update TestAccount.java

--- Chapter 9/9.7/TestAccount.java
+++ Chapter 9/9.7/TestAccount.java
@@ -1,4 +1,7 @@
-/* Test Account class */
+/* Test Account class 
+ * Data fields are set to private in Account class
+ * Getters and setters must be used to change and view values
+ */
 
 package account;
 
@@ -6,12 +9,12 @@ public class TestAccount {
 	/*  MAIN */ 
 	public static void main(String[] args) {
 		Account account = new Account();				// create account object
-		double monthlyInterest;						    	// create monthly interest variable
-		account.setId(1122);						      	// value sent to function -- set variable to value
-		account.setBalance(20000);					  	// value sent to function -- set variable to value
+		double monthlyInterest;						// create monthly interest variable
+		account.setId(1122);						// value sent to function -- set variable to value
+		account.setBalance(20000);					// value sent to function -- set variable to value
 		account.setInterestRate(0.045);					// value sent to function -- set variable to value
-		account.withdraw();								      // withdraw preset amount
-		account.deposit(); 							      	// deposit preset amount
+		account.withdraw();						// withdraw preset amount
+		account.deposit(); 						// deposit preset amount
 
 		monthlyInterest = account.getMonthlyInterest(account.getMonthlyInterestRate());
 		System.out.println("Account balance $" + account.getBalance() + "\n" + 
