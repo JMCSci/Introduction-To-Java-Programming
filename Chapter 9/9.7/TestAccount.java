b606e53aeead28379a3bb7043e39ed429fdb05a6
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-05 16:31:49 -0400

    Create TestAccount.java

---
+++ Chapter 9/9.7/TestAccount.java
@@ -0,0 +1,23 @@
+/* Test Account class */
+
+package account;
+
+public class TestAccount {
+	/*  MAIN */ 
+	public static void main(String[] args) {
+		Account account = new Account();				// create account object
+		double monthlyInterest;						    	// create monthly interest variable
+		account.setId(1122);						      	// value sent to function -- set variable to value
+		account.setBalance(20000);					  	// value sent to function -- set variable to value
+		account.setInterestRate(0.045);					// value sent to function -- set variable to value
+		account.withdraw();								      // withdraw preset amount
+		account.deposit(); 							      	// deposit preset amount
+
+		monthlyInterest = account.getMonthlyInterest(account.getMonthlyInterestRate());
+		System.out.println("Account balance $" + account.getBalance() + "\n" + 
+							"Monthly interest $" + monthlyInterest + "\n" + 
+							 "Account created: " + account.dateCreated());
+		
+	}
+
+}
