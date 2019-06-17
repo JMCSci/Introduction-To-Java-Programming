ce583182db77856944eff18db1d38fce14a2f12b
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-16 20:24:03 -0400

    Add Savings.java to 11.3

---
+++ Chapter 11/11.3/Savings.java
@@ -0,0 +1,50 @@
+/* SavingsAccount extends Account class
+ * Class models a savings account
+ */
+
+package subAccount;
+
+import account.Account;
+
+public class SavingsAccount extends Account {
+	double savBalance; 
+	double startingBalance;
+	
+	/* Cannot used no-arg constructor since has private data fields
+	 * Argument constructor must be invoked
+	 * Super used to invoke superclass methods and constructors
+	 * Default numbers 0 and 0 are used 
+	 */
+	
+	public SavingsAccount() {
+		super(0,0);
+	}
+	
+// getter
+	public double getSavings() {
+		return savBalance;
+	}
+	
+// setter
+	public void setSavings(double savBalance) {
+		this.savBalance = savBalance;
+	}
+	
+// method saves initial balance
+	public void initialBalance() {
+		startingBalance = savBalance; 
+	}
+	
+	
+// method determines if savings is overdrawn
+	public String overDrawn(double balance) {
+		if(balance < 0) {
+			savBalance = startingBalance; 
+			return "\nInsufficient funds\n" 
+					+ "Balance: $" + savBalance; 
+		} 
+		savBalance = balance;
+		return "Savings balance: $" + balance;
+	}
+
+}
