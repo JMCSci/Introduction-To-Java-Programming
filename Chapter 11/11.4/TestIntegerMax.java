1c470bc341906b61ad6bfe85145f368214f4a1ea
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-21 18:41:49 -0400

    Add TestIntegerMax.java to 11.4

---
+++ Chapter 11/11.4/TestIntegerMax.java
@@ -0,0 +1,31 @@
+/* Chapter 11.4
+ * 
+ */
+
+package integerMax;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestIntegerMax {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<>();
+		System.out.println("Enter a sequence of numbers. The last number should be 0: ");	
+		int x;
+
+		/* asks for user input
+		 * checks if number is not equal to 0
+		 * if not, number is add to object
+		 */
+		do {	
+			x = input.nextInt();
+			if(x != 0) 
+				list.add(x);
+
+		} while(x != 0);
+		System.out.println(IntegerMax.max(list));
+		input.close();
+	}
+}
+
