cff7e5ced89c0f2145c0dbafe59fcd4881f39a07
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-07-12 17:53:32 -0400

    Add TestLoan.java to 12.4

---
+++ Chapter 12/12.4/TestLoan.java
@@ -0,0 +1,13 @@
+/* Chapter 12.4
+ * Test program for Loan class 
+ */
+
+package illegalexception;
+
+public class TestLoan {
+	public static void main(String[] args) {
+		Loan test1 = new Loan(25,0,1000); 
+		Loan test2 = new Loan(25,1,1000);
+	}
+
+}
