750d0afb684713079d42f225c2c6cb3d6175ba9d
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-11-13 16:51:51 -0500

    Update GeometricFigures.java

--- Chapter 16/16.2/GeometricFigures.java
+++ Chapter 16/16.2/GeometricFigures.java
@@ -1,18 +1,14 @@
 /* Chapter 16.2
  * Program draws various geometric figures
- * User selects a figure from a radio button and uses a check box to specify if it is filled
+ * User selects a figure from radio button and uses a check box to specify if it is filled
  */
 
 package geometricfigures;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -27,50 +23,49 @@ import javafx.stage.Stage;
 
 public class GeometricFigures extends Application {
 	public void start(Stage primaryStage) {
+		// StackPane object to hold HBox and BorderPane
 		StackPane root = new StackPane();
-		
+		// BorderPane object
 		BorderPane bpane = new BorderPane();
-		
+		// HBox to hold RadioButtons and CheckBox
 		HBox hbox = new HBox();
 		hbox.setPadding(new Insets(10,10,10,10));
 		hbox.setSpacing(10);
 		bpane.setBottom(hbox);
-		hbox.setStyle("-fx-border-color:red;");
+		hbox.setStyle("-fx-border-color:black;");
 		hbox.setAlignment(Pos.CENTER);
-		
+		// RadioButton objects
 		RadioButton rb1 = new RadioButton("Circle");
 		RadioButton rb2 = new RadioButton("Rectangle");
 		RadioButton rb3 = new RadioButton("Ellipse");
-		
+		// ToggleGroup to group RadioButtons
 		ToggleGroup group = new ToggleGroup();
 		rb1.setToggleGroup(group);
 		rb2.setToggleGroup(group);
 		rb3.setToggleGroup(group);
-		
+		// CheckBox object
 		CheckBox cb = new CheckBox("Fill");
-		
+		// Shape objects (circle, rectangle, ellipse)
 		Circle circle = new Circle(50);
-		
+		Rectangle rectangle = new Rectangle(100, 100);
+		Ellipse ellipse = new Ellipse(50, 50, 75, 50);
+		// Event handler for shapes when RadioButton is selected
 		rb1.setOnAction(e -> {
 			circle.setFill(Color.TRANSPARENT);
 			circle.setStroke(Color.BLUE);
 			bpane.setCenter(circle);	
 		});
-		
 		rb2.setOnAction(e -> {
-			Rectangle rectangle = new Rectangle(100, 100);
 			rectangle.setFill(Color.TRANSPARENT);
 			rectangle.setStroke(Color.BLUE);
 			bpane.setCenter(rectangle);
 		});
-		
 		rb3.setOnAction(e -> {
-			Ellipse ellipse = new Ellipse(50, 50, 75, 50);
 			ellipse.setFill(Color.TRANSPARENT);
 			ellipse.setStroke(Color.BLUE);
 			bpane.setCenter(ellipse);
 		});
-		
+		// Event handlers for CheckBox (fill)
 		cb.setOnAction(e -> {
 			if(rb1.isSelected() && cb.isSelected() == true) {
 				circle.setFill(Color.BLUE);
@@ -78,16 +73,27 @@ public class GeometricFigures extends Application {
 			if(rb1.isSelected() && cb.isSelected() == false) {
 				circle.setFill(Color.TRANSPARENT);
 			}
+			if(rb2.isSelected() && cb.isSelected() == true) {
+				rectangle.setFill(Color.BLUE);
+			}
+			if(rb2.isSelected() && cb.isSelected() == false) {
+				rectangle.setFill(Color.TRANSPARENT);
+			}
+			if(rb3.isSelected() && cb.isSelected() == true) {
+				ellipse.setFill(Color.BLUE);
+			}
+			if(rb3.isSelected() && cb.isSelected() == false) {
+				ellipse.setFill(Color.TRANSPARENT);
+			}
+			
 		});
-		
-		
+		// Add objects to ObservableList (HBox and StackPane)
 		hbox.getChildren().addAll(rb1, rb2, rb3, cb);
 		root.getChildren().addAll(bpane);
-		
+		// Add StackPane to scene
 		Scene scene = new Scene(root, 400, 200);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
 	}
 	
 	public static void main(String[] args) {
