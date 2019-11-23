bf4ccb72797ab1dc2ce6f69eb37710e1b14cc3c9
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-11-22 21:10:17 -0500

    Update LetterHistogram.java
    Completed exercise

--- Chapter 16/16.11/LetterHistogram.java
+++ Chapter 16/16.11/LetterHistogram.java
@@ -3,6 +3,7 @@
  * File name entered in a text field 
  * Pressing ENTER key in text field causes program to start
  * File is processed and histogram is displayed in center of window
+ * Shows how to create a GUI in another class and send pane to main method
  */
 
 /* INSTRUCTIONS
@@ -17,11 +18,6 @@ package letterhistogram;
 
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -31,31 +27,19 @@ import javafx.stage.Stage;
  * BorderPane center will hold histogram Pane
  * BorderPane bottom will hold HBox 
  * HBox will hold TextField, Label, Button
- * 
- * Create EVENTS for text field and button
  */
 
 public class LetterHistogram extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane bpane = new BorderPane();
 		
+		// Create counts object
 		Counts counts = new Counts();
-		
-		HBox hbox = new HBox();
-		hbox.setSpacing(2);
-		hbox.setPadding(new Insets(0,0,2,0));
-		
-		Label lb1 = new Label("Filename");
-		
-		TextField tf = new TextField();
-		tf.setPrefWidth(325);
-		
-		Button b1 = new Button("View");
-		
-		hbox.getChildren().addAll(lb1, tf, b1);
+	
+		// Set BorderPane center with pane created in Counts class
 		bpane.setCenter(counts.getPane());
-		bpane.setBottom(hbox);
 		
+		// Create a new scene
 		Scene scene = new Scene(bpane, 435, 350);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Letter Frequency Histogram");
