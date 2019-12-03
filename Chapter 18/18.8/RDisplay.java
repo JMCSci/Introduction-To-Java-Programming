38eb102996f07ea0938083b653057e130229175f
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-12-02 20:55:53 -0500

    Add RDisplay.java to 18.8

---
+++ Chapter 18/18.8/RDisplay.java
@@ -0,0 +1,33 @@
+/* RDisplay class */
+
+package recursiveinteger;
+
+public class RDisplay {
+	protected char removed = ' ';
+	protected int newInt = 0;
+	protected int stringLength = 0;
+	protected String reverseNumber = "";
+	protected String temp = "";
+	protected String charTemp = "";
+	
+	/* METHOD: reverseDisplay */
+	public void reverseDisplay(int value) {
+		temp = Integer.toString(value);
+		stringLength = temp.length();
+		// BASE -- stopping condition
+		if(stringLength == 1) {
+			reverseNumber = reverseNumber.concat(temp);
+			newInt = Integer.parseInt(temp);
+			System.out.println(reverseNumber);
+		} else {
+			removed = temp.charAt(stringLength - 1);
+			charTemp = Character.toString(removed);
+			reverseNumber = reverseNumber.concat(charTemp);
+			newInt = Integer.parseInt(temp.substring(0, stringLength - 1));
+			reverseDisplay(newInt);
+		}
+	}
+	
+	
+
+}
