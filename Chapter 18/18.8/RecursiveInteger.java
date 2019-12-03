a48ffb1753ae95ee01ce34e1cf92edcccdcb82d9
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-12-02 20:55:16 -0500

    Add RecursiveInteger.java to 18.8

---
+++ Chapter 18/18.8/RecursiveInteger.java
@@ -0,0 +1,15 @@
+/* Abstract class for RecursiveInteger
+ * Not necessary but want to try something different
+ */
+
+package recursiveinteger;
+
+public abstract class RecursiveInteger {
+
+	protected RecursiveInteger() {
+		
+	}
+	
+	public abstract double reverseDisplay();
+
+}
