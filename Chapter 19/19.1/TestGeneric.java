736a638cbd971d70543e0d386d6d508c9cc10eae
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-01-06 17:15:51 -0500

    Update TestGeneric.java

--- Chapter 19/19.1/TestGeneric.java
+++ Chapter 19/19.1/TestGeneric.java
@@ -1,19 +1,47 @@
-/* Test ReviseGeneric */
+/* Test ReviseGeneric 
+ * TestGeneric is long b/c I tested every contingency 
+ */
 
 package revisegeneric;
 
 public class TestGeneric {
 	public static void main(String[] args) {
+		// Static methods and instance variables so class is accessed in static way
 		ReviseGeneric generic = new ReviseGeneric();
 		System.out.println("Current array values: ");
 		ReviseGeneric.printArray();
 		System.out.println();
+		System.out.println("Peek: " + ReviseGeneric.peek());
+		System.out.println();
 		System.out.println("Array size: " + ReviseGeneric.getSize());
-		ReviseGeneric.newArray();
+		ReviseGeneric.push("Madrid");
+		System.out.println();
 		System.out.println("Array size: " + ReviseGeneric.getSize());
 		System.out.println();
-		ReviseGeneric.push("Madrid");
 		System.out.println("New array values after push: ");
+		ReviseGeneric.printArray();
+		System.out.println();
+		ReviseGeneric.push("Venice");
+		System.out.println();
+		System.out.println("New array values after push: ");
+		ReviseGeneric.printArray();
+		ReviseGeneric.push("Stockholm");
+		System.out.println();
+		System.out.println("New array values after push: ");
+		ReviseGeneric.printArray();
+		ReviseGeneric.push("Warsaw");
+		System.out.println();
+		System.out.println("New array values after push: ");
+		ReviseGeneric.printArray();
+		System.out.println();
+		ReviseGeneric.push("Prague");
+		System.out.println();
+		System.out.println("New array values after push: ");
+		ReviseGeneric.printArray();
+		System.out.println();
+		System.out.println("Pop value: " + ReviseGeneric.pop());
+		System.out.println();
+		System.out.println("New array values after pop:");
 		ReviseGeneric.printArray();
 		System.out.println();
 		System.out.println("Pop value: " + ReviseGeneric.pop());
